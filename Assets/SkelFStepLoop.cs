3d5793679adb82365d46b79961c540624908d7e0
Alexandros Kalfoglou <foxtrot117@hotmail.com>
2019-02-03 23:41:25 +0200

    Most Pl sounds, fireDeam sounds, Golem behave and special skill, golem sounds not included

---
+++ Assets/SkelFStepLoop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkelFStepLoop : MonoBehaviour {
+
+    int numberOfSkel = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SkelEnemy") {
+            numberOfSkel++;
+            if (numberOfSkel >= 5) {
+                AudioManager.GetInstance().SetIfLoopingFsteps(true);
+                Debug.Log("more than or 7");
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "SkelEnemy" && numberOfSkel > 0) {
+            numberOfSkel--;
+            if (numberOfSkel < 5)
+            {
+                Debug.Log("less than 7");
+                AudioManager.GetInstance().SetIfLoopingFsteps(false);
+            }
+        }
+    }
+}
---
+++ Assets/torusOnColl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torusOnColl : MonoBehaviour {
+
+    [SerializeField]
+    int torusDamage = 1;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == ("Player")) {
+            MyGameManager.GetInstance().GetPlayer().GetComponent<PlayerStats>().IncDecHealth(-torusDamage);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            MyGameManager.GetInstance().GetPlayer().GetComponent<PlayerStats>().IncDecHealth(-torusDamage);
+        }
+    }
+
+
+}
